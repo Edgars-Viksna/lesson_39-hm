1688e92ed6579c2a6822b33b0d04c5ca2b2e8d42
Edgars-Viksna <edgars.viksna@hotmail.com>
2025-05-20 17:23:52 +0200

    Task 1 ready.

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> integerList = List.of(-3, -1, 0, 1, 3, 5);
+
+        System.out.println(maxCloseTo(integerList, 7));
+        System.out.println(maxCloseTo(integerList, 2));
+        System.out.println(maxCloseTo(integerList, -4));
+
+
+    }
+
+    public static String maxCloseTo(List<Integer> list, Integer integer) {
+        TreeSet<Integer> treeSet = new TreeSet<>(list);
+        Integer result1 = treeSet.lower(integer);
+        Integer result2 = treeSet.ceiling(integer);
+        String toLower = result1 != null ? result1.toString() : "none";
+        String toHigher = result2 != null ? result2.toString() : "none";
+        return "closest elements to " + integer + " are: " + toLower + " and " + toHigher;
+
+    }
+}
